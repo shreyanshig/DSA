be106dad02f5e72fcaaeaa063e7f40301c4de564
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-08-18 20:55:14 +0530

    Time: 4 ms (61.36%) | Memory: 50.3 MB (56.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int dfs(int grid[][], int row, int col, int n, int m, int dp[][])
-    {
-        if(row == n-1 && col == m-1)
-            return grid[row][col];
-        if(dp[row][col] != -1)
-            return dp[row][col];
-        int right = (int)1e9;
-        if(col+1 < m)
-            right = dfs(grid, row, col+1, n, m, dp);
-        int down = (int)1e9;
-        if(row+1 < n)
-            down = dfs(grid, row+1, col, n, m, dp);
-        dp[row][col] = grid[row][col] + Math.min(right, down);
-        return grid[row][col] + Math.min(right, down);   
-    }
     public int minPathSum(int[][] grid)
     {
-        int dp[][] = new int[grid.length][grid[0].length];
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                dp[i][j] = -1;
+        int n = grid.length;
+        int m = grid[0].length;
+        int dp[][] = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dp[i][j] = 0;
             }
         }
-        int a = dfs(grid, 0, 0, grid.length, grid[0].length, dp);
-        return a;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i == 0 && j == 0)
+                    dp[i][j] = grid[i][j];
+                else{
+                    int left = (int)1e9;
+                    int up = (int)1e9;
+                    if(j-1 >= 0)
+                        left = grid[i][j] + dp[i][j-1];
+                    if(i-1 >= 0)
+                        up = grid[i][j] + dp[i-1][j];
+                    dp[i][j] = Math.min(left, up);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
