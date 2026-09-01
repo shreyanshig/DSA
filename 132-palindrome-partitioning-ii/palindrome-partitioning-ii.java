77444642535b8a8f42dfa7a7fef58744c4fa84e0
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-09-01 16:04:48 +0530

    Time: 2494 ms (17.66%) | Memory: 42.6 MB (87.88%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -26,9 +26,16 @@ class Solution {
     public int minCut(String s)
     {
         int n = s.length();
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        int a = dfs(0, s.length(), s, dp);
-        return a-1;
+        int dp[] = new int[n+1];
+        dp[n] = 0;
+        for(int i=n-1; i>=0; i--){
+            int min = (int)1e9;
+            for(int j=i; j<n; j++){
+                if(isPalin(s, i, j))
+                    min = Math.min(min, 1 + dp[j+1]);
+            }
+            dp[i] = min;
+        }
+        return dp[0] - 1;
     }
 }
