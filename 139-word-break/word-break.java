2ce7b205f36cb262c528a95fcd860f6afa3eca09
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-09-01 14:30:44 +0530

    Time: 10 ms (11.20%) | Memory: 45.9 MB (63.12%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,34 +1,21 @@
 class Solution {
-    public boolean dfs(String s, int ind, int n, HashSet<String> hs, int dp[])
-    {
-         if(ind == n)
-            return true;
-         if(dp[ind] != -1)
-            return dp[ind] == 1;
-         for(int j=ind; j<n; j++){
-            String word = s.substring(ind, j+1);
-            if(hs.contains(word)){
-                boolean tf = dfs(s, j+1, n, hs, dp);
-                if(tf){
-                    dp[ind] = 1;
-                    return true;
-                }
-            }
-         }
-        dp[ind] = 0;
-        return false;
-    }
     public boolean wordBreak(String s, List<String> wordDict)
     {
+        int n = s.length();
         HashSet<String> hs = new HashSet<String>();
         for(String temp: wordDict){
             hs.add(temp);
         }
-        int dp[] = new int[s.length()];
-        for(int i=0; i<s.length(); i++){
-           dp[i] = -1;
+        boolean dp[] = new boolean[n+1];
+        dp[n] = true;
+        for(int ind=n-1; ind>=0; ind--){
+            for(int j=ind; j<n; j++){
+                if(hs.contains(s.substring(ind, j+1)) && dp[j+1] == true){
+                    dp[ind] = true;
+                    break;
+                }
+            }
         }
-        boolean tf = dfs(s, 0, s.length(), hs, dp);
-        return tf;
+        return dp[0];
     }
 }
