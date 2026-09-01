f77f86b7de369745ea9d44d953a5ff98800c4e6e
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-09-01 13:44:37 +0530

    Time: 10 ms (11.20%) | Memory: 46.3 MB (22.48%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean dfs(String s, int i, int j, int n, HashSet<String> hs, int dp[][])
+    {
+         if(j >= n){
+            if(i >= n)
+                return true;
+            return false;
+         }
+         if(dp[i][j] != -1)
+            return dp[i][j] == 1;
+         boolean notPart = dfs(s, i, j+1, n, hs, dp);
+         boolean part = false;
+         if(hs.contains(s.substring(i, j+1)))
+            part = dfs(s, j+1, j+1, n, hs, dp);
+        dp[i][j] = ((notPart | part) ? 1 : 0);
+        return notPart | part;
+    }
+    public boolean wordBreak(String s, List<String> wordDict)
+    {
+        HashSet<String> hs = new HashSet<String>();
+        for(String temp: wordDict){
+            hs.add(temp);
+        }
+        int dp[][] = new int[s.length()][s.length()];
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<s.length(); j++){
+                dp[i][j] = -1;
+            }
+        }
+        boolean tf = dfs(s, 0, 0, s.length(), hs, dp);
+        return tf;
+    }
+}
