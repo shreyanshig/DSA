6654821c1d8fe541a66995dea6529c8b827a779b
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-09-23 15:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (19.45%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,9 +2,9 @@ class Solution {
     public int rob(int[] nums)
     {
         int n = nums.length;
-        int next = 0;
+        int next = nums[n-1];
         int nextToNext = 0;
-        for(int ind=n-1; ind>=0; ind--){
+        for(int ind=n-2; ind>=0; ind--){
             int notTake = next;
             int take = nums[ind] + nextToNext;
             nextToNext = next;
