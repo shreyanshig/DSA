6d4e664e3ef828b6dd51572d583970040b0e99ae
Shreyanshi Golhani <108853285+shreyanshig@users.noreply.github.com>
2026-09-25 15:27:52 +0530

    Time: 1 ms (17.16%) | Memory: 42.8 MB (99.59%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,19 +1,4 @@
 class Solution {
-    public int dfs(int obstacleGrid[][], int row, int col, int n, int m, int dp[][])
-    {
-        if(row == n-1 && col == m-1)
-            return 1;
-        if(dp[row][col] != -1)
-            return dp[row][col];
-        int right = 0;
-        if(col + 1 < m && obstacleGrid[row][col+1] == 0)
-            right = dfs(obstacleGrid, row, col+1, n, m, dp);
-        int down = 0;
-        if(row + 1 < n && obstacleGrid[row+1][col] == 0)
-            down = dfs(obstacleGrid, row+1, col, n, m, dp);
-        dp[row][col] = right + down;
-        return right + down;
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid)
     {
         int n = obstacleGrid.length;
@@ -21,12 +6,20 @@ class Solution {
         if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1)
             return 0;
         int dp[][] = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                dp[i][j] = -1;
+        dp[n-1][m-1] = 1;
+        for(int row=n-1; row>=0; row--){
+            for(int col=m-1; col>=0; col--){
+                if(row == n-1 && col == m-1)
+                    continue;
+                int right = 0;
+                if(col + 1 < m && obstacleGrid[row][col+1] == 0)
+                    right = dp[row][col+1];
+                int down = 0;
+                if(row + 1 < n && obstacleGrid[row+1][col] == 0)
+                    down = dp[row+1][col];
+                dp[row][col] = right + down;
             }
         }
-        int a = dfs(obstacleGrid, 0, 0, n, m, dp);
-        return a;
+        return dp[0][0];
     }
 }
